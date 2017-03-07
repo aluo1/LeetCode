755c3a2da8007cc3e98749966d81b1385f670b10
Martin Luo <aluo1@student.unimelb.edu.au>
2017-03-07 17:43:52 +1100

    Copy data structure of tree from LeetCode

---
+++ MaximumDepthOfBinTree/src/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * Author: An Luo
+ * email: aluo1.developer@gmail.com
+ * <p>
+ * Problem Website: https://leetcode.com/problems/maximum-depth-of-binary-tree
+ * <p>
+ * Description: Definition for a binary tree node.
+ * Acknowledgement: This code is provided by LeetCode.
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
