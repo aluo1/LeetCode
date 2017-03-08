795c0a1eb8ac085fda06ad171564e92d30d2e4c7
Martin Luo <aluo1@student.unimelb.edu.au>
2017-03-08 14:14:32 +1100

    Create a private method to set up the roman-to-integer dictionary

--- RomanToInteger/src/com/aluo1/leetcode/easy/Main.java
+++ RomanToInteger/src/com/aluo1/leetcode/easy/Main.java
@@ -1,12 +1,40 @@
 package com.aluo1.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        romanToInt("X");
     }
 
-    public int romanToInt(String s) {
+    public static int romanToInt(String s) {
+        Map<String, Integer> romanToIntDic = getRomanToIntDic();
+
+        System.out.println(romanToIntDic);
+
         return 0;
+    }
+
+    /* a private method to create a dictionary for roman to integer */
+    private static Map<String, Integer> getRomanToIntDic() {
+        Map<String, Integer> romanToIntDic = new HashMap<String, Integer>();
+
+        romanToIntDic.put("I", 1);
+        romanToIntDic.put("IV", 4);
+        romanToIntDic.put("V", 5);
+        romanToIntDic.put("IX", 9);
+        romanToIntDic.put("X", 10);
+        romanToIntDic.put("XL", 40);
+        romanToIntDic.put("L", 50);
+        romanToIntDic.put("XC", 90);
+        romanToIntDic.put("C", 100);
+        romanToIntDic.put("CD", 400);
+        romanToIntDic.put("D", 500);
+        romanToIntDic.put("CM", 900);
+        romanToIntDic.put("M", 1000);
+
+        return romanToIntDic;
     }
 }
