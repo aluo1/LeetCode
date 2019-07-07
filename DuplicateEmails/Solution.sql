20bbf8a05bc936a41804e25d7e46bc6c8b2307f7
An (Martin) Luo <aluo1.developer@gmail.com>
2019-07-07 23:23:41 +0800

    update comment style

--- DuplicateEmails/Solution.sql
+++ DuplicateEmails/Solution.sql
@@ -1,8 +1,8 @@
 /*
-182. Duplicate Emails
-Difficulty: Easy
-执行用时: 1489 ms , 在所有 MS SQL Server 提交中击败了 76.61% 的用户
-内存消耗: N/A
+    182. Duplicate Emails
+    Difficulty: Easy
+    执行用时: 1489 ms , 在所有 MS SQL Server 提交中击败了 76.61% 的用户
+    内存消耗: N/A
 */
 SELECT Email FROM 
 (
