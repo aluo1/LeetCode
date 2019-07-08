f7c4326ce367977378af04aa4800f4f54714b5df
An (Martin) Luo <aluo1.developer@gmail.com>
2019-07-08 11:28:28 +0800

    first try of max depth of bst

---
+++ MaxDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {        
+        if (root.left == null && root.right == null) {
+            // Leaf
+            return 1;
+        }
+        
+        if (root.left != null && root.right == null) {
+            return 1 + MaxDepth(root.left);
+        }
+        
+        if (root.left == null && root.right != null) {
+            return 1 + MaxDepth(root.right);
+        }
+        
+        int leftBranchDepth = MaxDepth(root.left);
+        int rightBranchDepth = MaxDepth(root.right);
+
+        return 1 + (leftBranchDepth > rightBranchDepth ? leftBranchDepth : rightBranchDepth);
+    }
+}
