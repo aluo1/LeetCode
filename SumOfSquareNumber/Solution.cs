5101f5a94b83377da24edfe25febb9da7ca31fdb
An (Martin) Luo <aluo1.developer@gmail.com>
2019-07-11 20:42:54 +0800

    Finally resolve the problem

--- SumOfSquareNumber/Solution.cs
+++ SumOfSquareNumber/Solution.cs
@@ -1,24 +1,21 @@
+/// Question 633. Sum of Square Numbers
+/// Difficulty: Easy
+/// 执行用时: 68 ms, 在所有 C# 提交中击败了 61.54% 的用户
+/// 内存消耗: 12.7 MB, 在所有 C# 提交中击败了 44.44% 的用户
+
 public class Solution {
     public bool JudgeSquareSum(int c) {
         int sqrtOfC = (int) Math.Ceiling(Math.Sqrt(c));
-        int breakPoint = sqrtOfC/2+1;
                 
-        for (int i = breakPoint; i >= 0; i--) {
-            for (int j = 0; j <= breakPoint; j++) {
-                if (i*i + j*j == c) {
-                    return true;
-                }
+        for (int i = 0; i <= sqrtOfC; i++) {
+            int remainingValue = c - i * i;
+            double sqrtOfRemaining = Math.Sqrt(remainingValue);
+            if (Math.Ceiling(sqrtOfRemaining) == sqrtOfRemaining && 
+                Math.Floor(sqrtOfRemaining) == sqrtOfRemaining) {
+                return true;
             }
         }
-        
-        for (int i = sqrtOfC; i >= 0; i--) {
-            for (int j = 0; j <= sqrtOfC; j++) {
-                if (i*i + j*j == c) {
-                    return true;
-                }
-            }
-        }
-        
+
         return false;
     }
 }
