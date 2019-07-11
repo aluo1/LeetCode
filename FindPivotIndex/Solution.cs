02c05cc351b2bd5bb796b197e05295eae1689c85
An (Martin) Luo <aluo1.developer@gmail.com>
2019-07-11 21:30:51 +0800

    try to fix the timeout, but now got error

--- FindPivotIndex/Solution.cs
+++ FindPivotIndex/Solution.cs
@@ -1,11 +1,41 @@
 public class Solution {
     public int PivotIndex(int[] nums) {
+        int pivotIndex = -1;
         int midOfTheArray = (int) Math.Ceiling(nums.Length / 2d);
 
         // Start from the mid, move towards start of the array.
-        for (int i = 0; i < nums.Length; i++) {
+        for (int i = midOfTheArray; i > 0; i--) {
             int sumOfLeft = 0;
             int sumOfRight = 0;
+            bool broken = false;
+            
+            for (int j = 0; j < i; j++) {
+                sumOfLeft += nums[j];
+            }
+            
+            // this is the right most index, skip.
+            if (i + 1 > nums.Length) {
+                continue;
+            }
+            
+            for (int j = i + 1; j < nums.Length; j++) {
+                sumOfRight += nums[j];
+            }
+            
+            if (sumOfLeft == sumOfRight) {
+                pivotIndex = i;
+            }
+        }
+        
+        if (pivotIndex != -1) {
+            return pivotIndex;
+        }
+        
+        // Start from the mid, move towards end of the array.
+        for (int i = midOfTheArray; i < nums.Length; i++) {
+            int sumOfLeft = 0;
+            int sumOfRight = 0;
+            bool broken = false;
             
             for (int j = 0; j < i; j++) {
                 sumOfLeft += nums[j];
