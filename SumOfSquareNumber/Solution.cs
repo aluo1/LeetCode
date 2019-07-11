d45c31761df14c3550e423c887493fb1571bfa7c
An (Martin) Luo <aluo1.developer@gmail.com>
2019-07-11 10:26:52 +0800

    Introduce breakpoint to resolve the timeout issue, but failed

--- SumOfSquareNumber/Solution.cs
+++ SumOfSquareNumber/Solution.cs
@@ -1,8 +1,10 @@
 public class Solution {
     public bool JudgeSquareSum(int c) {
         int sqrtOfC = (int) Math.Ceiling(Math.Sqrt(c));
-        for (int i = sqrtOfC; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
+        int breakPoint = sqrtOfC/2+1;
+                
+        for (int i = breakPoint; i >= 0; i--) {
+            for (int j = 0; j <= breakPoint; j++) {
                 if (i*i + j*j == c) {
                     return true;
                 }
