0a902b688a7d63f8c681c0cd6bcb789b8b16776b
An (Martin) Luo <aluo1.developer@gmail.com>
2019-07-11 10:20:57 +0800

    another failed as timeout

--- SumOfSquareNumber/Solution.cs
+++ SumOfSquareNumber/Solution.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public bool JudgeSquareSum(int c) {
-        int sqrtOfC = (int) Math.Sqrt(c);
-        for (int i = 0; i <= sqrtOfC; i++) {
-            for (int j = sqrtOfC; j >= 0; j--) {
+        int sqrtOfC = (int) Math.Ceiling(Math.Sqrt(c));
+        for (int i = sqrtOfC; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
                 if (i*i + j*j == c) {
                     return true;
                 }
