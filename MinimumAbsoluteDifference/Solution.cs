1d4b9949cd4a16e50c02b98ccd00900eaa63d44e
An (Martin) Luo <aluo1.developer@gmail.com>
2020-03-03 14:23:25 +0800

    pass the test

--- MinimumAbsoluteDifference/Solution.cs
+++ MinimumAbsoluteDifference/Solution.cs
@@ -1,50 +1,32 @@
+/// Question 1200. Minimum Absolute Difference
+/// Difficulty: Easy
+/// 执行用时: 420 ms, 在所有 C# 提交中击败了 50.00% 的用户
+/// 内存消耗: 42.6 MB, 在所有 C# 提交中击败了 33.33% 的用户
+
 public class Solution {
     public IList<IList<int>> MinimumAbsDifference(int[] arr) {
         int minAbsDiff = arr.Max();
-        Dictionary<string, List<List<int>>> absDiffMapping = new Dictionary<string, List<List<int>>>();
+        int[] sortedArray = arr;
+        Array.Sort(sortedArray);
+        
+        IList<IList<int>> minCombo = new List<IList<int>>();
 
-        int arraySize = arr.Length;
-        for (int i = 0; i < arraySize; i++) {
-            for (int j = 0; j < arraySize; j++) {
-                if (i == j) { continue; }
+        for (int i = 0; i < sortedArray.Length-1; i++) {
+            int iVal = sortedArray[i];
+            int jVal = sortedArray[i+1];
 
-                int iVal = arr[i];
-                int jVal = arr[j];
-                
-                int absDiff = iVal - jVal;
-                List<int> currentCombo = new List<int>{jVal, iVal};
-                if (absDiff < 0) {
-                    absDiff *= -1;
-                    currentCombo = new List<int>{iVal, jVal};
-                }
-
-                if (minAbsDiff > absDiff) {
-                    minAbsDiff = absDiff;
-                }
-
-                string absDiffString = absDiff.ToString();
-                List<List<int>> currentValue = absDiffMapping.ContainsKey(absDiffString) ? absDiffMapping[absDiffString] : new List<List<int>>();
-
-                if (!currentValue.Any(currentVal => currentVal[0] == currentCombo[0] && currentVal[1] == currentCombo[1])) {
-                    currentValue.Add(currentCombo);
-                }
-                
-                absDiffMapping[absDiffString] = currentValue;
-            }
+            int currentDiff = Math.Abs(jVal - iVal);
+            
+            if (currentDiff == minAbsDiff) {
+                minCombo.Add(new List<int>{iVal, jVal});
+            } 
+            
+            if (currentDiff < minAbsDiff) {
+                minAbsDiff = currentDiff;
+                minCombo = new List<IList<int>>{ new List<int>{iVal, jVal}};
+            }    
         }
 
-        IList<IList<int>> iList = new List<IList<int>>();
-        List<List<int>> minAbsDiffList = absDiffMapping[minAbsDiff.ToString()];
-
-        minAbsDiffList.Sort((firstVal, secondVal) => {
-            if (firstVal[0] == secondVal[0]) {
-                return firstVal[1].CompareTo(secondVal[1]);
-            } else {
-                return firstVal[0].CompareTo(secondVal[0]);
-            }
-        });
-        minAbsDiffList.ForEach(val => iList.Add(val));
-
-        return iList;
+        return minCombo;
     }
 }
