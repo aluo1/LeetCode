8e2c48521260dfa179aa119954e8df960dd65948
An (Martin) Luo <aluo1.developer@gmail.com>
2020-03-05 10:37:55 +0800

    add two more solutions

--- DistributeCandiesToPeople/Solution.cs
+++ DistributeCandiesToPeople/Solution.cs
@@ -1,11 +1,16 @@
 
 /// Question 1103. Distribute Candies to People
 /// Difficulty: Easy
-/// 执行用时: 340 ms, 在所有 C# 提交中击败了 60.00% 的用户
-/// 内存消耗: 24 MB, 在所有 C# 提交中击败了 100.00% 的用户
 
 public class Solution {
     public int[] DistributeCandies(int candies, int num_people) {
+        return DistributeCandiesFirstTry(candies, num_people);
+    }
+
+    public int[] DistributeCandiesFirstTry(int candies, int num_people) {
+        /// 执行用时: 240 ms, 在所有 C# 提交中击败了 66.67% 的用户
+        /// 内存消耗: 25.7 MB, 在所有 C# 提交中击败了 25.00% 的用户
+
         int[] candiesDistribution = new int[num_people];
         int currentIndex = 0;
         
@@ -22,5 +27,48 @@ public class Solution {
         }
 
         return candiesDistribution;
+    }
+
+    public int[] DistributeCandiesSecondTry(int candies, int num_people) {
+        /// 执行用时: 332 ms
+        /// 内存消耗: 25.7 MB
+        int remainingCandies = candies;
+        int[] distributedCandies = new int[num_people];
+        int iterationCount = 0;
+
+        while (remainingCandies > 0) {
+            for (int i = 0; i < num_people; i++) {
+                int expectedCandies = iterationCount * num_people + i + 1;
+                int candiesToDistribute = expectedCandies > remainingCandies ? remainingCandies : expectedCandies;
+
+                remainingCandies -= candiesToDistribute;
+                
+                distributedCandies[i] += candiesToDistribute;
+            }
+            iterationCount++;
+        }
+
+        return distributedCandies;
+    }
+
+    public int[] DistributeCandiesThirdTry(int candies, int num_people) {
+        /// 执行用时: 244 ms, 在所有 C# 提交中击败了 53.33% 的用户
+        /// 内存消耗: 25.6 MB, 在所有 C# 提交中击败了 25.00% 的用户
+        int[] distributedCandies = new int[num_people];
+        int iterationCount = 0;
+
+        while (candies > 0) {
+            for (int i = 0; i < num_people; i++) {
+                int expectedCandies = iterationCount * num_people + i + 1;
+                int candiesToDistribute = expectedCandies > candies ? candies : expectedCandies;
+
+                candies -= candiesToDistribute;
+                
+                distributedCandies[i] += candiesToDistribute;
+            }
+            iterationCount++;
+        }
+
+        return distributedCandies;
     }
 }
