4e6b7427dd48bb4f6064601784cf289441a10ab6
An (Martin) Luo <aluo1.developer@gmail.com>
2020-03-09 10:51:35 +0800

    add official solution

--- BestTimeToBuyAndSellStock/Solution.cs
+++ BestTimeToBuyAndSellStock/Solution.cs
@@ -3,6 +3,10 @@
 
 public class Solution {
     public int MaxProfit(int[] prices) {
+        return MaxProfitSecondTry(prices);
+    }
+
+    public int MaxProfitFirstTry(int[] prices) {
         // 执行用时: 1520 ms, 在所有 C# 提交中击败了 5.36% 的用户
         // 内存消耗: 25.3 MB, 在所有 C# 提交中击败了 5.95% 的用户
 
@@ -16,6 +20,27 @@ public class Solution {
             }
         }
         
+        return maxProfit;
+    }
+
+    public int MaxProfitSecondTry(int[] prices) {
+        // 执行用时: 108 ms, 在所有 C# 提交中击败了 88.47% 的用户
+        // 内存消耗: 25.5 MB, 在所有 C# 提交中击败了 5.95% 的用户
+        //
+        // Acknowledgement: This code is borrowed from official solution (https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/121-mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode-/).
+        int maxProfit = 0;
+        int minValue = int.MaxValue;
+
+        for (int i = 0; i < prices.Length; i++) {
+           if (prices[i] < minValue) {
+               minValue = prices[i];
+           }
+
+           if (prices[i] - minValue > maxProfit) {
+               maxProfit = prices[i] - minValue;
+           }
+        }
+
         return maxProfit;
     }
 }
