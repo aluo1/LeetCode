6a9be33405a1df77a0979ec132cb7c71e7b4c280
An (Martin) Luo <aluo1.developer@gmail.com>
2020-04-08 19:07:34 +0800

    q303

---
+++ Easy/303 - RangeSumQuery/Solution.cs
@@ -0,0 +1,59 @@
+/// Question 303. Range Sum Query - Immutable (https://leetcode-cn.com/problems/range-sum-query-immutable/)
+
+public class NumArray
+{
+    /// 执行用时 : 344 ms, 在所有 C# 提交中击败了 40.63% 的用户
+    /// 内存消耗 : 35.7 MB, 在所有 C# 提交中击败了 25.00% 的用户
+
+    private readonly int[] nums;
+
+    public NumArray(int[] nums)
+    {
+        this.nums = nums;
+    }
+
+    public int SumRange(int i, int j)
+    {
+        int sum = 0;
+        for (int index = i; index <= j; index++)
+        {
+            sum += this.nums[index];
+        }
+
+        return sum;
+    }
+}
+
+public class NumArrayWithPrecalculatedValue
+{
+    /// 执行用时 : 1116 ms, 在所有 C# 提交中击败了 6.25% 的用户
+    /// 内存消耗 : 35.7 MB, 在所有 C# 提交中击败了 25.00% 的用户
+
+    private readonly int[] rangeSum;
+
+    public NumArray(int[] nums)
+    {
+        this.rangeSum = new int[nums.Count()];
+
+        for (int i = 0; i < nums.Count(); i++)
+        {
+            this.rangeSum[i] = nums.Take(i + 1).Sum();
+        }
+    }
+
+    public int SumRange(int i, int j)
+    {
+        if (i == 0)
+        {
+            return this.rangeSum[j];
+        }
+
+        return this.rangeSum[j] - this.rangeSum[i - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(i,j);
+ */
