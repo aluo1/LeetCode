59e9d759a2a8a3b0faba6d56a5256f997e3685cb
An (Martin) Luo <aluo1.developer@gmail.com>
2020-04-15 15:49:30 +0800

    replace the list with dictionary to avoid timeout

--- Medium/542 - 01Matrix/Solution.cs
+++ Medium/542 - 01Matrix/Solution.cs
@@ -1,3 +1,7 @@
+/// Question 542. 01 Matrix (https://leetcode-cn.com/problems/01-matrix/)
+/// 执行用时: 564 ms, 在所有 C# 提交中击败了 27.78% 的用户
+/// 内存消耗: 48.4 MB, 在所有 C# 提交中击败了 100.00% 的用户
+
 public class Solution
 {
     private readonly List<(int, int)> DIRECTIONS = new List<(int, int)>()
@@ -13,7 +17,7 @@ public class Solution
         int[][] distanceMatrix = new int[matrixRowSize][];
 
         // A list storing the visited node.
-        List<(int, int)> visitedCooridnates = new List<(int, int)>();
+        Dictionary<(int, int), bool> visitedCooridnates = new Dictionary<(int, int), bool>();
         Queue<(int, int)> queue = new Queue<(int, int)>();
 
         // Visit all zeros first.
@@ -25,7 +29,7 @@ public class Solution
             {
                 if (matrix[i][j] == 0)
                 {
-                    visitedCooridnates.Add((i, j));
+                    visitedCooridnates[(i, j)] = true;
                     queue.Enqueue((i, j));
                     distanceRow[j] = 0;
                 }
@@ -34,26 +38,27 @@ public class Solution
             distanceMatrix[i] = distanceRow;
         }
 
-        while (queue.Count > 0)
+        while (queue.Any())
         {
             (int row, int column) = queue.Dequeue();
-            foreach ((int rowDirection, int columnDirection) in this.DIRECTIONS)
+
+            for (int i = 0; i < this.DIRECTIONS.Count(); i++)
             {
+                (int rowDirection, int columnDirection) = this.DIRECTIONS.ElementAt(i);
+
                 int newRow = row + rowDirection;
                 int newColumn = column + columnDirection;
 
-                if (newRow < 0 ||
-                    newRow >= matrixRowSize ||
-                    newColumn < 0 ||
-                    newColumn >= matrixColumnSize ||
-                    visitedCooridnates.Exists(record => record.Item1.Equals(newRow) && record.Item2.Equals(newColumn)))
+                if (newRow >= 0 &&
+                    newRow < matrixRowSize &&
+                    newColumn >= 0 &&
+                    newColumn < matrixColumnSize &&
+                    !visitedCooridnates.ContainsKey((newRow, newColumn)))
                 {
-                    continue;
+                    distanceMatrix[newRow][newColumn] = distanceMatrix[row][column] + 1;
+                    visitedCooridnates[(newRow, newColumn)] = true;
+                    queue.Enqueue((newRow, newColumn));
                 }
-
-                distanceMatrix[newRow][newColumn] = distanceMatrix[row][column] + 1;
-                visitedCooridnates.Add((newRow, newColumn));
-                queue.Enqueue((newRow, newColumn));
             }
         }
 
