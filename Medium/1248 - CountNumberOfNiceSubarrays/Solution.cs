a25b4828ffda78abc12ef1ee9743ab2216e52ea0
An (Martin) Luo <aluo1.developer@gmail.com>
2020-04-21 09:55:03 +0800

    official solution reimplementation

--- Medium/1248 - CountNumberOfNiceSubarrays/Solution.cs
+++ Medium/1248 - CountNumberOfNiceSubarrays/Solution.cs
@@ -2,33 +2,30 @@
 
 public class Solution
 {
+    /// 执行用时: 300 ms, 在所有 C# 提交中击败了 50.00% 的用户
+    /// 内存消耗: 44 MB, 在所有 C# 提交中击败了 100.00% 的用户
+    /// Acknowledgement: This idea is borrowed from the [official solution](https://leetcode-cn.com/problems/count-number-of-nice-subarrays/solution/tong-ji-you-mei-zi-shu-zu-by-leetcode-solution/).
     public int NumberOfSubarrays(int[] nums, int k)
     {
-        int count = 0;
-        int arrayLength = nums.Count();
+        // Initialize with -1 at index 0
+        List<int> oddNumsIndex = new List<int>() { -1 };
 
-        for (int i = 0; i < arrayLength; i++)
+        for (int i = 0; i < nums.Count(); i++)
         {
-            int oddCount = 0;
-            for (int j = i; j < arrayLength; j++)
+            if (nums[i] % 2 == 1)
             {
-                int currentNumber = nums[j];
-                if (currentNumber % 2 != 0)
-                {
-                    oddCount += 1;
-                }
-
-                if (oddCount == k)
-                {
-                    count++;
-                }
-                else if (oddCount > k)
-                {
-                    break;
-                }
+                oddNumsIndex.Add(i);
             }
         }
 
-        return count;
+        oddNumsIndex.Add(nums.Count());
+
+        int arrayCount = 0;
+        for (int i = 1; i < oddNumsIndex.Count() - k; i++)
+        {
+            arrayCount += (oddNumsIndex.ElementAt(i) - oddNumsIndex.ElementAt(i - 1)) * (oddNumsIndex.ElementAt(i + k) - oddNumsIndex.ElementAt(i + k - 1));
+        }
+
+        return arrayCount;
     }
 }
