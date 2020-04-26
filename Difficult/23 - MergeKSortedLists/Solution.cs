bdc4e5f06f26e47e7bd5ad6e73a05018fb493d20
An (Martin) Luo <aluo1.developer@gmail.com>
2020-04-26 14:11:54 +0800

    add official solution in

--- Difficult/23 - MergeKSortedLists/Solution.cs
+++ Difficult/23 - MergeKSortedLists/Solution.cs
@@ -65,3 +65,52 @@ public class Solution
         return mergedSortedLinkedList;
     }
 }
+
+
+public class Solution
+{
+    // 执行用时: 132 ms, 在所有 C# 提交中击败了 85.98% 的用户
+    // 内存消耗: 29.4 MB, 在所有 C# 提交中击败了 50.00% 的用户
+    // Acknowledgement: This idea borrows from the [official solution](https://leetcode-cn.com/problems/merge-k-sorted-lists/solution/he-bing-kge-pai-xu-lian-biao-by-leetcode-solutio-2/).
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+        return this.Merge(lists, 0, lists.Count() - 1);
+    }
+
+    private ListNode Merge(ListNode[] lists, int left, int right)
+    {
+        if (left > right) { return null; }
+        if (left == right) { return lists[left]; }
+
+        int mid = (left + right) / 2;
+
+        return this.MergeTwoLists(Merge(lists, left, mid), Merge(lists, mid + 1, right));
+    }
+
+    private ListNode MergeTwoLists(ListNode lists1, ListNode lists2)
+    {
+        if (lists1 == null || lists2 == null) { return lists1 == null ? lists2 : lists1; }
+        ListNode head = new ListNode();
+        ListNode tail = head;
+
+        while (lists1 != null && lists2 != null)
+        {
+            if (lists1.val < lists2.val)
+            {
+                tail.next = lists1;
+                lists1 = lists1.next;
+            }
+            else
+            {
+                tail.next = lists2;
+                lists2 = lists2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        tail.next = lists1 != null ? lists1 : lists2;
+
+        return head.next;
+    }
+}
