c9fdeb14dd8e3cf08cc1e935c5458297699d5d86
An (Martin) Luo <aluo1.developer@gmail.com>
2020-04-28 14:56:31 +0800

    add the iteration approach solution

--- Easy/700 - SearchInABinarySearchTree/Solution.cs
+++ Easy/700 - SearchInABinarySearchTree/Solution.cs
@@ -32,3 +32,19 @@ public class Solution
         }
     }
 }
+
+public class SolutionWithIteration
+{
+    // 执行用时: 144 ms, 在所有 C# 提交中击败了 62.86% 的用户
+    // 内存消耗: 34 MB, 在所有 C# 提交中击败了 100.00% 的用户
+    // Acknowledgement: This solution is borrowed from the [official solution](https://leetcode-cn.com/problems/search-in-a-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-sou-suo-by-leetcode/)
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        while (root != null && root.val != val)
+        {
+            root = root.val > val ? root.left : root.right;
+        }
+
+        return root;
+    }
+}
