97d989def2a2dbc3b514dd4fb5f37398baafa4ab
An (Martin) Luo <aluo1.developer@gmail.com>
2020-05-01 23:00:14 +0800

    Q21 rewrite

--- Easy/21 - MergeTwoSortedLists/Solution.cs
+++ Easy/21 - MergeTwoSortedLists/Solution.cs
@@ -1,7 +1,5 @@
 // Question 21. Merge Two Sorted Lists (https://leetcode-cn.com/problems/merge-two-sorted-lists/)
 // Difficulty: Easy
-// 执行用时: 108 ms, 在所有 C# 提交中击败了 82.64% 的用户
-// 内存消耗: 25.9 MB, 在所有 C# 提交中击败了 5.71% 的用户
 
 /**
  * Definition for singly-linked list.
@@ -13,6 +11,9 @@
  */
 public class Solution
 {
+    // 执行用时: 108 ms, 在所有 C# 提交中击败了 82.64% 的用户
+    // 内存消耗: 25.9 MB, 在所有 C# 提交中击败了 5.71% 的用户
+
     public ListNode MergeTwoLists(ListNode l1, ListNode l2)
     {
         if (l1 == null) { return l2; }
@@ -73,6 +74,44 @@ public class Solution
                 break;
             }
         }
+
+        return mergedNode;
+    }
+}
+
+
+public class Solution
+{
+    // 执行用时: 104 ms, 在所有 C# 提交中击败了 93.70% 的用户
+    // 内存消耗: 25.8 MB, 在所有 C# 提交中击败了 25.00% 的用户
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+        if (l1 == null) { return l2; }
+        if (l2 == null) { return l1; }
+
+        ListNode mergedNode = l1.val > l2.val ? l2 : l1;
+        ListNode currentNode = mergedNode;
+
+        while (l1 != null && l2 != null)
+        {
+            ListNode swappingNode;
+
+            if (l1.val > l2.val)
+            {
+                swappingNode = l2;
+                l2 = l2.next;
+            }
+            else
+            {
+                swappingNode = l1;
+                l1 = l1.next;
+            }
+
+            currentNode.next = swappingNode;
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = l1 == null ? l2 : l1;
 
         return mergedNode;
     }
