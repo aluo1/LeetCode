0a80c4d19f8237f7715b178f9af9f47f7b40f5b8
An (Martin) Luo <aluo1.developer@gmail.com>
2020-09-30 13:17:38 +0800

    Q145 in iteration

--- Medium/145 - BinaryTreePostorderTraversal/solution.ts
+++ Medium/145 - BinaryTreePostorderTraversal/solution.ts
@@ -40,3 +40,53 @@ function postorderTraversal(root: TreeNode | null): number[] {
       ]
     : [];
 }
+
+/**
+ * 执行用时：92 ms, 在所有 typescript 提交中击败了 12.68% 的用户
+ * 内存消耗：39.6 MB, 在所有 typescript 提交中击败了 6.25% 的用户
+ *
+ * @param {(TreeNode | null)} root
+ * @returns {number[]}
+ */
+function postorderTraversalUsingStack(root: TreeNode | null): number[] {
+  if (!root) {
+    return [];
+  }
+
+  const traversed: number[] = [];
+  const stack: (TreeNode | null)[] = [];
+  let prevNode: TreeNode | null = null;
+
+  while (root || stack.length) {
+    // Add all left children to stacks first.
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+
+    root = stack.pop() ?? null;
+
+    console.log(`root.right.val = ${root?.right?.val ?? "null"}`);
+    console.log(stack);
+
+    if (root && (!root.right || (prevNode && root.right === prevNode))) {
+      console.log(
+        `line 35, root.val = ${root?.val ?? "null"}, root.right.val = ${
+          root?.right?.val ?? "null"
+        }\n`
+      );
+      traversed.push(root.val);
+      prevNode = root;
+      root = null;
+    } else {
+      console.log(`line 43, root.val = ${root?.val ?? "null"}`);
+
+      stack.push(root);
+      root = root?.right ?? null;
+
+      console.log(`line 47, root.val = ${root?.val ?? "null"}\n`);
+    }
+  }
+
+  return traversed;
+}
