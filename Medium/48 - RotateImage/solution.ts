173062b38bc9b0808f8a7ae9eec03036cbfddd47
Martin Luo <aluo1.developer@gmail.com>
2020-12-19 13:55:28 +0800

    add new rotating solution

--- Medium/48 - RotateImage/solution.ts
+++ Medium/48 - RotateImage/solution.ts
@@ -25,3 +25,33 @@ function rotate(matrix: number[][]): void {
     }
   }
 }
+
+/**
+ * 执行用时：88 ms, 在所有 TypeScript 提交中击败了 53.33% 的用户
+ * 内存消耗：40.2 MB, 在所有 TypeScript 提交中击败了 36.36% 的用户
+ * 
+ * Acknowledgement: This solution borrows idea from method II
+ * of the [official solution](https://leetcode-cn.com/problems/rotate-image/solution/xuan-zhuan-tu-xiang-by-leetcode-solution-vu3m/)
+ *
+ * @param {number[][]} matrix
+ */
+function rotate(matrix: number[][]): void {
+  const N: number = matrix.length;
+
+  // Rotate
+  for (let i = 0; i < Math.floor(N / 2); i++) {
+    for (let j = 0; j < Math.floor((N + 1) / 2); j++) {
+      [
+        matrix[i][j],
+        matrix[N - j - 1][i],
+        matrix[N - i - 1][N - j - 1],
+        matrix[j][N - i - 1],
+      ] = [
+        matrix[N - j - 1][i],
+        matrix[N - i - 1][N - j - 1],
+        matrix[j][N - i - 1],
+        matrix[i][j],
+      ];
+    }
+  }
+}
